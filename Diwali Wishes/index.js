e208b691717b414c889ef81d3a25ecd70bfbdc80
Vishwa-191999 <gvishwajeet487@gmail.com>
2024-09-18 15:07:16 +0530

    Add Html & Css pages

---
+++ Diwali Wishes/index.js
@@ -0,0 +1,11 @@
+
+// console.log("Hello world");
+// var message="   Hi this is vishwa    trim";
+// console.log(message.trim());
+
+var age=prompt("Enter ur age")
+var user="vishwa"
+
+if(age>=18)
+    console.log(`Hi, ${user} u r eligible`)
+else console.log(`Sorry ${user} u r not eligible`)
